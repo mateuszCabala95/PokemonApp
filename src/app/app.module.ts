adf554969190f034e1ceb75e3d69f66fb1f25ded
Mateusz <mateusz.cabala95@gmail.com>
2020-04-18 19:38:24 +0200

    Add default view in main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './Pages/main-page/main-page.component';
+import { NavigationComponent } from './Components/navigation/navigation.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { PokedexComponent } from './Pages/pokedex/pokedex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavigationComponent,
+    FooterComponent,
+    PokedexComponent
   ],
   imports: [
     BrowserModule,
