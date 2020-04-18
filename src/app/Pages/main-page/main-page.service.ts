ecd3d2cc1f3ead82be54414c9f5fc191eeddc22f
Mateusz <mateusz.cabala95@gmail.com>
2020-04-18 21:41:29 +0200

    Add handle error in MainPageService

--- src/app/Pages/main-page/main-page.service.ts
+++ src/app/Pages/main-page/main-page.service.ts
@@ -1,7 +1,8 @@
 import {Injectable, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
 import {Observable, throwError} from "rxjs";
 import {catchError, retry} from "rxjs/operators";
+import {PokemonModel} from "../../Models/Pokemon.model";
 
 
 @Injectable({
@@ -12,12 +13,30 @@ export class MainPageService {
   randomIndex: Number = Math.round(Math.random() * 964); // 964 - number of all pokemon
   PokemonURL: String = `https://pokeapi.co/api/v2/pokemon/${this.randomIndex.toString()}/`;
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
   constructor(private http: HttpClient) {
   }
 
-  getRandomPokemon = () => {
-    return this.http.get(this.PokemonURL.toString())
+  getRandomPokemon = ():Observable<PokemonModel> => {
+    return this.http.get<PokemonModel>(this.PokemonURL.toString()).pipe(
+      retry(3),
+      catchError(err =>this.handleError(err))
+    )
   }
 }
 
